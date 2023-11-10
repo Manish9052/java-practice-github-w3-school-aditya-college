5b2450a1e16ce183039646d9ee33d1457f955c47
Manish vishwakarma <124370063+Manish9052@users.noreply.github.com>
2023-11-10 06:28:38 +0000

    the code after the while with multiple statements

--- java_print_stmt_using_while_loop_10_times.java
+++ java_print_stmt_using_while_loop_10_times.java
@@ -1,3 +1,13 @@
 public class java_print_stmt_using_while_loop_10_times{
-    psvm
+    public static void main(String[] args) {
+        int i=0;
+        int no=0;
+        while(i<10&&no<10)
+    {
+System.out.println(no+"jay shree ram");
+i++;
+no++;
+    }
+        
+    }
 }
